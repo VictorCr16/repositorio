1e350820a1be995d842ecb6e4c610beb015cabe2
VitinQuebraCranio <victorcarvalho112008@gmail.com>
2024-10-01 16:16:02 -0300

    Add files via upload

---
+++ Victor.js
@@ -0,0 +1,31 @@
+
+let num1 = Number("Digite o primeiro número");
+let num2 = Number("Digito o segundo número");
+
+function Subtrair(num1, num2){
+    let resultado;
+    resultado1= num1 * num2;
+
+}
+console.log("O resultado é:" +resultado)
+ 
+
+//Booelean = true or false
+let matriculado = true;
+let naomatriculado = false
+
+//number = numero interno
+var idade = 15;
+
+//float = numero reais com decimais
+let altura = 1.76;
+
+//string = Caracter (letra)  
+const nome = "pedro"
+
+//objeto
+let pessoa = {
+    nome: "joão",
+    idade: 16,
+    altura: 1.89,
+}
